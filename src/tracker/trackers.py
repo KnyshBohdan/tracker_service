de80d29ab6784f53ab610db1349d6fc028247356
KnyshBohdan <110291685+KnyshBohdan@users.noreply.github.com>
2023-07-24 17:34:57 +0300

    Added trackers: Boosting TLD KCF MOSSE Also added but have some errors: GOTURN MedianFlow

--- src/tracker/trackers.py
+++ src/tracker/trackers.py
@@ -1,11 +1,22 @@
 """
 File what contain tracker implementations
+
+There are many copy&paste here because using trackers in OpenCV is quiet the same for all types of tracker
+The reason why I am broke DRY principle, because we cannot change the tracking function at the same time for
+all types of trackers, because each has its own tracking principle. Some look at a part of the image, others
+at the whole image
+
+TODO: find out correct approach
 """
 
 from src.tracker.base import TrackerBase
 import cv2
 import numpy as np
 
+
+###################
+# MIL
+###################
 
 class MILTracker(TrackerBase):
     def __init__(self, bbox: np.ndarray):
@@ -36,5 +47,174 @@ class MILTracker(TrackerBase):
             return None
 
     def set_bbox(self, bbox: np.ndarray):
+        self.bbox = bbox
+        self.is_initialized = False
+
+
+###################
+# BOOSTING
+###################
+
+class BoostingTracker(TrackerBase):
+    def __init__(self, bbox):
+        super().__init__(bbox)
+        self.tracker = cv2.legacy.TrackerBoosting_create()
+        self.is_initialized = False
+
+    def track(self, frame):
+        """
+        TODO: write description
+        :param frame: The frame in which to track the object.
+        :return: New bounding box (x, y, w, h) where the object is found in the frame. Return None if object is not found.
+        """
+        if not self.is_initialized:
+            self.tracker.init(frame, self.bbox)
+            self.is_initialized = True
+
+        success, bbox = self.tracker.update(frame)
+        return bbox if success else None
+
+    def set_bbox(self, bbox):
+        self.bbox = bbox
+        self.is_initialized = False
+
+
+###################
+# MEDIANFLOW
+###################
+
+class MedianFlowTracker(TrackerBase):
+    def __init__(self, bbox):
+        super().__init__(bbox)
+        self.tracker = cv2.legacy.TrackerMedianFlow_create()
+        self.is_initialized = False
+
+    def track(self, frame):
+        """
+        TODO: write description
+        :param frame: The frame in which to track the object.
+        :return: New bounding box (x, y, w, h) where the object is found in the frame. Return None if object is not found.
+        """
+        if not self.is_initialized:
+            self.tracker.init(frame, self.bbox)
+            self.is_initialized = True
+
+        success, bbox = self.tracker.update(frame)
+
+        return bbox if success else None
+
+    def set_bbox(self, bbox):
+        self.bbox = bbox
+        self.is_initialized = False
+
+
+###################
+# TLD
+###################
+
+class TLDTracker(TrackerBase):
+    def __init__(self, bbox):
+        super().__init__(bbox)
+        self.tracker = cv2.legacy.TrackerTLD_create()
+        self.is_initialized = False
+
+    def track(self, frame):
+        """
+        TODO: write description
+        :param frame: The frame in which to track the object.
+        :return: New bounding box (x, y, w, h) where the object is found in the frame. Return None if object is not found.
+        """
+        if not self.is_initialized:
+            self.tracker.init(frame, self.bbox)
+            self.is_initialized = True
+
+        success, bbox = self.tracker.update(frame)
+        return bbox if success else None
+
+    def set_bbox(self, bbox):
+        self.bbox = bbox
+        self.is_initialized = False
+
+
+###################
+# KCF
+###################
+
+class KCFTracker(TrackerBase):
+    def __init__(self, bbox):
+        super().__init__(bbox)
+        self.tracker = cv2.TrackerKCF_create()
+        self.is_initialized = False
+
+    def track(self, frame):
+        """
+        TODO: write description
+        :param frame: The frame in which to track the object.
+        :return: New bounding box (x, y, w, h) where the object is found in the frame. Return None if object is not found.
+        """
+        if not self.is_initialized:
+            self.tracker.init(frame, self.bbox)
+            self.is_initialized = True
+
+        success, bbox = self.tracker.update(frame)
+        return bbox if success else None
+
+    def set_bbox(self, bbox):
+        self.bbox = bbox
+        self.is_initialized = False
+
+
+###################
+# GOTURN
+###################
+
+class GOTURNTracker(TrackerBase):
+    def __init__(self, bbox):
+        super().__init__(bbox)
+        self.tracker = cv2.TrackerGOTURN_create()
+        self.is_initialized = False
+
+    def track(self, frame):
+        """
+        TODO: write description
+        :param frame: The frame in which to track the object.
+        :return: New bounding box (x, y, w, h) where the object is found in the frame. Return None if object is not found.
+        """
+        if not self.is_initialized:
+            self.tracker.init(frame, self.bbox)
+            self.is_initialized = True
+
+        success, bbox = self.tracker.update(frame)
+        return bbox if success else None
+
+    def set_bbox(self, bbox):
+        self.bbox = bbox
+        self.is_initialized = False
+
+
+###################
+# MOSSE
+###################
+
+class MOSSETracker(TrackerBase):
+    def __init__(self, bbox):
+        super().__init__(bbox)
+        self.tracker = cv2.legacy.TrackerMOSSE_create()
+        self.is_initialized = False
+
+    def track(self, frame):
+        """
+        TODO: write description
+        :param frame: The frame in which to track the object.
+        :return: New bounding box (x, y, w, h) where the object is found in the frame. Return None if object is not found.
+        """
+        if not self.is_initialized:
+            self.tracker.init(frame, self.bbox)
+            self.is_initialized = True
+
+        success, bbox = self.tracker.update(frame)
+        return bbox if success else None
+
+    def set_bbox(self, bbox):
         self.bbox = bbox
         self.is_initialized = False
--- tests/test_trackers.py
+++ tests/test_trackers.py
@@ -1,5 +1,6 @@
 import pytest
-from src.tracker.trackers import MILTracker
+from src.tracker.trackers import MILTracker, BoostingTracker, MOSSETracker,\
+    MedianFlowTracker, TLDTracker, KCFTracker, GOTURNTracker
 import numpy as np
 
 
@@ -24,3 +25,30 @@ class TestMILTracker:
 
         # Check if the tracker returns the correct bounding box
         assert bbox == (49, 52, 100, 100) # TODO: find out why is returning (49, 52, 100, 100) not [50, 50, 100, 100]
+
+
+tracker_classes = [MILTracker, BoostingTracker, TLDTracker, KCFTracker, MOSSETracker]
+# TODO: solve errors in GOTURNTracker
+# TODO: solve errors in MedianFlowTracker
+
+class TestTrackers:
+    @pytest.mark.parametrize("tracker_class", tracker_classes)
+    def test_creation(self, tracker_class):
+        tracker = tracker_class(np.array([50, 50, 100, 100]))
+        assert isinstance(tracker.bbox, np.ndarray)
+
+    @pytest.mark.parametrize("tracker_class", tracker_classes)
+    def test_track(self, tracker_class):
+        # Setup
+        tracker = tracker_class(np.array([50, 50, 100, 100]))
+
+        # Create a synthetic image
+        image = np.ones((200, 200, 3), dtype=np.uint8) * 255
+
+        # Draw a rectangle on the image that corresponds to the bbox
+        image[50:150, 50:150, :] = 0
+
+        bbox = tracker.track(image)
+
+        # Check if the tracker returns the correct bounding box
+        assert len(bbox) == 4
